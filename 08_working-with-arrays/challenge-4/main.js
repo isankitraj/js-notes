48b690481d3b1a1e58bd3bb37511d0aa9720e76f
Ankit <isankitraj@outlook.com>
2023-09-03 17:34:08 +0530

    added challenge 4

--- 08_working-with-arrays/challenge-4/main.js
+++ 08_working-with-arrays/challenge-4/main.js
@@ -19,17 +19,54 @@ dogs.forEach(dog => {
   dog.recommendedFood = dog.weight ** 0.75 * 28;
 });
 
-
-
-
 // task 2
-const okayAmount = function (current, recommendedFood) {
-  return current > recommendedFood * 0.9 && current < recommendedFood * 1.1;
-};
 const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'));
 console.log(
-  sarahDog.curFood > okayAmount(sarahDog.curFood, sarahDog.recommendedFood) ? "eating too much" : "eating too less"
+  sarahDog.curFood > sarahDog.recommendedFood
+    ? 'eating too much'
+    : 'eating too less'
 );
 
-// console.log(sarahDog);
+// task 3
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood)
+  .map(dog => dog.owners)
+  .flat();
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recommendedFood)
+  .flatMap(dog => dog.owners);
 
+console.log(ownersEatTooMuch);
+console.log(ownersEatTooLittle);
+
+// tast 4
+console.log(`${ownersEatTooMuch.join(' and ')} dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')} dogs eat too Little!`);
+
+// task 5
+const check = dogs.some(dog => dog.curFood === dog.recommendedFood);
+// console.log(check);
+
+// task 6
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recommendedFood * 0.9 &&
+  dog.curFood < dog.recommendedFood * 1.1;
+
+// const okay = dogs.some(
+//   dog =>
+//     dog.curFood > dog.recommendedFood * 0.9 &&
+//     dog.curFood < dog.recommendedFood * 1.1
+// );
+console.log(dogs.some(checkEatingOkay));
+
+
+// task 7
+const okayDogs = dogs.filter(checkEatingOkay).flatMap(dog => dog.owners)
+console.log(okayDogs);
+
+
+//task 8
+const shallowCopy = [...dogs]  // or you can use slice mehtod to create a shallow copy
+shallowCopy.sort((a,b) => a.recommendedFood - b.recommendedFood)
+console.log(shallowCopy);
+console.log(dogs);
